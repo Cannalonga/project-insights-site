d60686b044651060b2865d3aaa94bab256ad6a32
Cannalonga <l2requests@gmail.com>
2026-04-24 14:09:34 -0300

    refactor(landing): streamline home sections for Landing V2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,9 @@ import { FinalCTASection } from "@/components/FinalCTASection";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
-import { HowItWorksSection } from "@/components/HowItWorksSection";
 import { MockupSection } from "@/components/MockupSection";
-import { NotReplacementSection } from "@/components/NotReplacementSection";
 import { ProblemSection } from "@/components/ProblemSection";
 import { SolutionSection } from "@/components/SolutionSection";
-import { StructuredDataSection } from "@/components/StructuredDataSection";
-import { ValidationBeforePurchaseSection } from "@/components/ValidationBeforePurchaseSection";
 
 export default function Home() {
   return (
@@ -19,16 +15,12 @@ export default function Home() {
       <Header />
       <main>
         <HeroSection />
-        <ProblemSection />
         <CompatibilitySection />
+        <ProblemSection />
         <SolutionSection />
         <MockupSection />
         <DifferentialsSection />
-        <HowItWorksSection />
         <FeatureSection />
-        <StructuredDataSection />
-        <NotReplacementSection />
-        <ValidationBeforePurchaseSection />
         <FinalCTASection />
       </main>
       <Footer />
